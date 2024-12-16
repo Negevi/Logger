b9750b68c2cbea5bc4e5781431d3057a0bfc16aa
Negevi <lukenicklemke@gmail.com>
2024-12-16 17:27:40 -0300

    Some things

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod structs;
 fn main() {
-    println!("Hello World!");
-    let date = chrono::Local::now().date_naive();
-    println!("Hoje é dia: {date}")
+    let input = structs::get_input_i32(
+        "Welcome to the logger. What would you like to do?:
+    [0] Open settings
+    [1] Create a Log
+    [2] Exit code",
+    );
+    match input {
+        0 => println!("opening settings..."),
+        1 => println!("creating log..."),
+        2 => std::process::exit(1),
+        _ => println!("Ops, thats invalid!"),
+    }
+    let log = structs::Log::create(
+        structs::get_input("Name?"),
+        structs::Level::Info,
+        "This is a msg.".to_string(),
+    );
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,13 +1,15 @@
 pub struct Log {
     name: String,
-    date: chrono::Local,
+    date: chrono::NaiveDate,
     origin: String,
     level: Level,
     msg: String,
 }
 
 impl Log {
-    fn create(name: String, date: chrono::Local, origin: String, level: Level, msg: String) -> Log {
+    pub fn create(name: String, level: Level, msg: String) -> Log {
+        let date = chrono::Local::now().date_naive();
+        let origin: String = "origin".to_string();
         let log: Log = Log {
             name,
             date,
@@ -18,8 +20,7 @@ impl Log {
         return log;
     }
 }
-
-enum Level {
+pub enum Level {
     Info,
     Debug,
     Warning,
@@ -30,13 +31,26 @@ enum Error {
     InvalidType,
 }
 
-fn get_input(q: &str) -> String {
+pub fn get_input(q: &str) -> String {
     let mut input = String::new();
     println!("{q}");
-    std::io::stdin().read_line(&mut input);
-    return input;
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => input.trim().to_string(),
+        Err(_) => get_input(q),
+    }
+}
+
+pub fn get_input_i32(q: &str) -> i32 {
+    let mut input = String::new();
+    println!("{q}");
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => input.trim().parse::<i32>().unwrap(),
+        Err(_) => get_input_i32(q),
+    }
 }
 
 // let date = chrono::Local::now().date_naive();
 // this returns the system's local date.
 // DateTime<Local> parsed to YYYY-MM-DD
+
+// "files" path: D:\Rust\files
